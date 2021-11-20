c6f77d4b5aae0e2dad054d01e7fc4abdd680d73d
nyquist <nyquist@vaspun.eu>
2021-11-20 23:51:31 +0100

    Create dohoperator.mk

---
+++ networking/mikrotik/dohoperator.mk
@@ -0,0 +1,23 @@
+## Enables or disables doh on errors
+## /ip 
+
+:local disableMsg "Disabling DoH";
+:local enableMsg "Enabling DoH";
+:local dohErrors [:len [/log find where time>([/system clock get time] - 5m) and message~"DoH server connection error"]];
+:local dohStatus [/ip dns get use-doh-server];
+:local dohDisabledRecently [:len [/log find where time>([/system clock get time] - 5m) and message~"$disableMsg"]];
+#:log debug "debug: dohErrors: $dohErrors. dohStatus: $dohStatus. dohDisabledRecently: $dohDisabledRecently";
+:if ($dohErrors>0 && $dohStatus!="") \
+    do={ \
+        :log info "dohErrors: $dohErrors. dohStatus: $dohStatus";
+        :log info $disableMsg; 
+        :put [ip dns set use-doh-server=""];
+    } \
+    else={ \
+        :if ($dohStatus="" && $dohDisabledRecently=0)\
+            do={ \
+                :log info "dohErrors: $dohErrors. dohStatus: $dohStatus";
+                :log info $enableMsg;
+                :put [ip dns set use-doh-server="https://cloudflare-dns.com/dns-query"];
+            }    
+    }
